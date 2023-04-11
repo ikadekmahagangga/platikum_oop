c4ed182a27b43d40e340c807dc9e7512da081c23
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2023-04-11 09:54:13 +0800

    Create tugasnanti.java

---
+++ tugasnanti.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author INSTIKI
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[][] nama =
+        {
+            {"adi","Ami","Agus"},
+            {"Bela","Budi","Beny"},
+            {"Bela","Budi","Beny"},
+            {"Bela","Budi","Beny"}
+            
+        };
+        String[][] nim = 
+        {
+            {"12345","15221","32132"},
+            {"14324","12512","26342"}
+        };
+        String[][] jurusan =
+        {
+            {"mti","kab","paris","aus","ais"}
+        };
+        System.out.println(nama[0][0])+" "+(nim[1][0]));
+    }
+    
+}
