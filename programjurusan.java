920fbfefc859a2122979772ab72cd34fb9194fb4
I KADEK MAHA GANGGA <102144626+ikadekmahagangga@users.noreply.github.com>
2023-04-11 15:11:02 +0700

    Create programjurusan.java

---
+++ programjurusan.java
@@ -0,0 +1,26 @@
+public class Project {
+    public static void main(String[] args) 
+    {
+        String[][]nama =
+        {
+            {"Iqra Mullen","Claude Torres","Arthur Vaughn","Aleena Jennings"},
+            {"Fay Carr","Ronnie Small","Morgan Everett","Elsie Riddlea"}
+        };
+        String[][]jurusan =
+        {
+            {"MTI","KAB","DGM","PDI"},
+            {"KPR","DPR","REP","RIP"}
+        };
+        String[][]nim =
+        {
+            {"123421","23214","23214","21324"},
+            {"984013","93913","31294","31223"}
+        };
+        System.out.println("---------------program jurusan-------------------");
+        System.out.println((nama[0][0])+" "+(jurusan[0][0])+" "+(nim[0][0]));
+        System.out.println((nama[0][1])+" "+(jurusan[0][1])+" "+(nim[1][2]));
+        System.out.println((nama[0][2])+" "+(jurusan[0][2])+" "+(nim[0][3]));
+        System.out.println((nama[0][3])+" "+(jurusan[0][3])+" "+(nim[1][0])); 
+        System.out.println("---------------program jurusan-------------------");
+    }    
+}
